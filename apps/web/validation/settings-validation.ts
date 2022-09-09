29de8ccb5a0b4f3101f445f3f354cd66605d70e8
Abderraouf Zine <rofazayn@gmail.com>
2022-09-09 13:46:25 +0100

    fix(web): add nullable trait to some of validation items

--- apps/web/validation/settings-validation.ts
+++ apps/web/validation/settings-validation.ts
@@ -2,11 +2,15 @@ import * as Yup from 'yup'
 
 export const userSettingsSchema = Yup.object({
   name: Yup.string().min(5).max(20).required(),
-  phoneNumber: Yup.string().test('len', 'Invalid phone number', (val: any) => {
-    if (!val) return true
-    const val_length_without_dashes = val.replace(/-|_/g, '').length
-    return val_length_without_dashes === 18
-  }),
-  country: Yup.string().max(20),
-  job: Yup.string().max(20),
+  phoneNumber: Yup.string().test(
+    'len',
+    'Incorrect phone number',
+    (val: any) => {
+      if (!val) return true
+      const val_length_without_dashes = val.replace(/-|_/g, '').length
+      return val_length_without_dashes === 18
+    }
+  ),
+  country: Yup.string().max(20).nullable(true),
+  job: Yup.string().min(4).max(20).nullable(true),
 })
